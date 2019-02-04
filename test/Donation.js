a9cc7023daf5e6a07b41e681785957ee6b513f69
rohitsethii <rohitsethi836@gmail.com>
2019-02-04 23:18:11 +0530

    handled zeroAddrress

--- test/Donation.js
+++ test/Donation.js
@@ -6,13 +6,14 @@ contract('Donation', async (accounts) => {
     let val = 1 * ether;
     let amount = 11 * ether;
     let withdrawvalue = 500000000000;
+    let zeroAddress = "0x0000000000000000000000000000000000000000";
     
     beforeEach(async function() {
         let instance = await Donation.new();
     })
      it("should let donar to donate", async () => {
         let instance = await Donation.deployed();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:val});
+        let donate = await instance.donation(zeroAddress ,{from:accounts[0],value:val});
         let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
         let referral = await instance.referred.call(accounts[1]);    
         assert.equal(referral,accounts[0]);            
@@ -21,7 +22,7 @@ contract('Donation', async (accounts) => {
      it("should let donar to donate funds and check the reffered address", async () => {
         let instance = await Donation.new();
         const contractbalance1 = web3.eth.getBalance(instance.address).toNumber();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:val});
+        let donate = await instance.donation("zeroAddress",{from:accounts[0],value:val});
         let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
         let donate2 = await instance.donation(accounts[1],{from:accounts[2],value:val});
         let donate3 = await instance.donation(accounts[2],{from:accounts[3],value:val});
@@ -32,7 +33,7 @@ contract('Donation', async (accounts) => {
 
      it("should let donar to donate funds and check the reffered address", async () => {
         let instance = await Donation.new();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[4],value:val});
+        let donate = await instance.donation("zeroAddress",{from:accounts[4],value:val});
         let donate1 = await instance.donation(accounts[4],{from:accounts[1],value:val});
         let donate2 = await instance.donation(accounts[1],{from:accounts[2],value:val});
         let donate3 = await instance.donation(accounts[2],{from:accounts[3],value:val});
@@ -43,7 +44,7 @@ contract('Donation', async (accounts) => {
 
      it("should let donar to donate funds and check the reffered address", async () => {
         let instance = await Donation.new();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:val});
+        let donate = await instance.donation("zeroAddress",{from:accounts[0],value:val});
         let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
         let donate2 = await instance.donation(accounts[1],{from:accounts[2],value:val});
         let donate3 = await instance.donation(accounts[1],{from:accounts[3],value:val});
@@ -53,7 +54,7 @@ contract('Donation', async (accounts) => {
      });
      it("should let donar to donate funds and check all the participants", async () => {
         let instance = await Donation.new();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:val});
+        let donate = await instance.donation("zeroAddress",{from:accounts[0],value:val});
         let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
         let donate2 = await instance.donation(accounts[1],{from:accounts[2],value:val});
         let donate3 = await instance.donation(accounts[2],{from:accounts[3],value:val});
@@ -65,7 +66,7 @@ contract('Donation', async (accounts) => {
      });
      it("should let owner withdraw ethers and check remaining balance", async () => {
         let instance = await Donation.new();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:val});
+        let donate = await instance.donation("zeroAddress",{from:accounts[0],value:val});
         let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
         let donate2 = await instance.donation(accounts[1],{from:accounts[2],value:val});
         let donate3 = await instance.donation(accounts[2],{from:accounts[3],value:val});
@@ -80,7 +81,7 @@ contract('Donation', async (accounts) => {
      });
      it("shows the total number ethers in contract", async () => {
         let instance = await Donation.new();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:val});
+        let donate = await instance.donation("zeroAddress",{from:accounts[0],value:val});
         let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
         let donate2 = await instance.donation(accounts[1],{from:accounts[2],value:val});
         let donate3 = await instance.donation(accounts[2],{from:accounts[3],value:val});
@@ -92,7 +93,7 @@ contract('Donation', async (accounts) => {
      it("should throw error when donar try to refer himself", async () => {
         try{
             let instance = await Donation.new();
-            let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:amount});
+            let donate = await instance.donation("zeroAddress",{from:accounts[0],value:amount});
             let donate1 = await instance.donation(accounts[0],{from:accounts[0],value:val});
             }
         catch(err){
@@ -103,7 +104,7 @@ contract('Donation', async (accounts) => {
      it("should throw error when donar try to donate more than 10 ethers", async () => {
         try{
             let instance = await Donation.new();
-            let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:amount});
+            let donate = await instance.donation("zeroAddress",{from:accounts[0],value:amount});
             let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
             let referral = await instance.referred.call(accounts[1]);    
             }
@@ -114,7 +115,7 @@ contract('Donation', async (accounts) => {
     it("reverts when donar try to donate less than 1 ethers", async () => {
         try{
             let instance = await Donation.new();
-            let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:99999999999999999});
+            let donate = await instance.donation("zeroAddress",{from:accounts[0],value:99999999999999999});
             let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
             let referral = await instance.referred.call(accounts[1]);    
             }
@@ -124,7 +125,7 @@ contract('Donation', async (accounts) => {
     });
     it("should throw error when withdrawl amount is greater than balance in contract", async () => {
         let instance = await Donation.new();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:val});
+        let donate = await instance.donation("zeroAddress",{from:accounts[0],value:val});
         let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
         let donate2 = await instance.donation(accounts[1],{from:accounts[2],value:val});
         let donate3 = await instance.donation(accounts[2],{from:accounts[3],value:val});
@@ -139,7 +140,7 @@ contract('Donation', async (accounts) => {
     });
     it("should throw error when others try to withdraw amount", async () => {
         let instance = await Donation.new();
-        let donate = await instance.donation("0x0000000000000000000000000000000000000000",{from:accounts[0],value:val});
+        let donate = await instance.donation("zeroAddress",{from:accounts[0],value:val});
         let donate1 = await instance.donation(accounts[0],{from:accounts[1],value:val});
         let donate2 = await instance.donation(accounts[1],{from:accounts[2],value:val});
         let donate3 = await instance.donation(accounts[2],{from:accounts[3],value:val});
